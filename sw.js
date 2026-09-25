d7b709787327da6565762820ad194a26838a3f86
ziadmohamed-crypto <ziadmohamedooo4@gmail.com>
2026-09-25 23:33:50 +0300

    Update service worker cache version and constants
    new version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="schedule-v2",F=["./","./index.html","./manifest.json","./icon.svg","./icon-192.png","./icon-512.png"];
+const C="schedule-merged-v4",F=["./","./index.html","./manifest.json","./icon.svg","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
